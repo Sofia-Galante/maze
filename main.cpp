13c729dd168d37a70c4ca89456192f3020c82dc4
Sofia Galante <sofia.galante@stud.unifi.it>
2022-02-06 10:10:36 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <omp.h>
+
+//TODO: algoritmo che generi un labirinto valido.
+
+//TODO: algoritmo che risolva il labirinto in modo sequenziale
+//TODO: algoritmo che risolva il labirinto in modo parallelo
+
+int main() {
+    return 0;
+}
